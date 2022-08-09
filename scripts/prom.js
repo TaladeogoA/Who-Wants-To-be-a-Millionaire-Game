07d2bc5a8d514303649ad0eea3ef4f5a7d7d5a72
Tobi Balogun <tobi.balo@gmail.com>
2022-08-09 11:25:35 +0100

    Initial attempt to modify program control flow

---
+++ scripts/prom.js
@@ -0,0 +1,5 @@
+//1. First get json data from the URL using fetch API
+//2. Combine all the question sets into one array
+//3. Create function that randomly selects a question object from the array
+//4. Create a function that will take the question object and create a question card
+//5. Create a function that will take the question card and display it on the page
